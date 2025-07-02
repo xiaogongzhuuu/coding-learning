8a582f8cc3c006eec6a844b87a2a38079cc18aa9
xiaogongzhuuu <caosiyuan123456789@163.com>
2025-07-02 18:47:56 +0800

    backend

--- backend/main.py
+++ backend/main.py
@@ -2,4 +2,12 @@ from fastapi import FastAPI
 app=FastAPI()
 @app.get("/")
 def index():
-    return "hello fastapi"
+    return{"wellcome to my homepage"}
+@app.get("/user/{name}")
+def getname(name):
+    return {"hello" + name}
+@app.get("/square/{number}")
+def square(number):
+    number=int(number)
+    return{"result":number*number}
+
