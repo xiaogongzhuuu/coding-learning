d31f1061c06eb0bfac717d48e6e657092a15d54b
xiaogongzhuuu <caosiyuan123456789@163.com>
2025-07-10 06:55:42 +0000

    ,

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 #uvicorn backend.main:app
 from typing import Annotated
-from fastapi import FastAPI,Path,Query
+from fastapi import FastAPI,Path,Query,Body
 from fastapi.responses import RedirectResponse , FileResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -36,18 +36,23 @@ def square(number:Annotated[int,None]):
     number=int(number)
     return{"result":number*number}
 
-
-@app.get("/test")
-def gettest():
-    return {"get":True}
-
-@app.post("/test")
-def posttest():
-    return {"post":True}
-
-# app.mount("/",StaticFiles(directory="frontend",html=True))
 @app.get("/")
 def home():
     return FileResponse("frontend/index.html")
 
+# get 请求
+@app.get("/test")
+def gettest():
+    return {"get":True}
 
+#post 请求
+@app.post("/test")
+def posttest(body=Body(None)):
+    print(body)
+    return {"post":True}
+
+
+
+
+
+# app.mount("/",StaticFiles(directory="frontend",html=True))
